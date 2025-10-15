ded69816872e6bdc4fd57c1fbdea1ae21c18a0c7
zations <pavithran1146@gmail.com>
2025-10-15 23:34:43 +0530

    (feature) feat(model): add tag field to Note (nullable)

--- core/models.py
+++ core/models.py
@@ -3,6 +3,7 @@ from django.db import models
 class Note(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
+    tag = models.CharField(max_length=50, null=True, blank=True)  # NEW
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
