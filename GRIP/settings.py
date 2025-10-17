6c13c98b434bc2869cdad39a0e517af859ca8dcc
Abhinav-AR38 <abhinavar38@gmail.com>
2025-10-17 23:09:33 +0530

    (error) regression(static): incorrect STATICFILES_DIRS causes collectstatic failure
    Visible: ImproperlyConfigured on collectstatic
    
    Root cause: invalid STATICFILES_DIRS path.

--- GRIP/settings.py
+++ GRIP/settings.py
@@ -124,4 +124,6 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / "core" / "static"]
+STATICFILES_DIRS = ["wrong/path/static"]
+
+
