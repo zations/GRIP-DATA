cdb3bfbaa7f4b2ac6da207f73bc59174c2748f07
Abhinav-AR38 <abhinavar38@gmail.com>
2025-10-17 23:04:04 +0530

    (feature) feat(ui): add app.css and wire static in base

--- GRIP/settings.py
+++ GRIP/settings.py
@@ -122,3 +122,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR / "core" / "static"]
